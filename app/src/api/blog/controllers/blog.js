885aba79915deaa3e94f0138112a8f7f7e80358d
Asadbek_Azamjonov <asadbek.azamjonov@devkom.pl>
2024-03-03 02:31:37 +0100

    initial commit

---
+++ app/src/api/blog/controllers/blog.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * blog controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blog.blog', ({ strapi }) => ({
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.db.query('api::blog.blog').findOne({ 
+            where: { slug: id },    
+            populate: ['thumbnail'],
+        });
+
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+    },
+
+    sanitizeOutput(entity, ctx) {
+        // Define your sanitization logic here
+        return entity;
+    },
+
+    transformResponse(sanitizedEntity) {
+        // Define your response transformation logic here
+        return sanitizedEntity;
+    },
+}));
+// 
---
+++ app/src/api/blog/routes/blog.js
@@ -0,0 +1,8 @@
+'use strict';
+/**
+ * blog router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blog.blog');
